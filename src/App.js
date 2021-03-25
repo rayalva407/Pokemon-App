c7f86965358604d6923c0a308a34886504974cf7
rayalva407 <rayalva407@gmail.com>
2021-03-25 14:58:29 -0600

    sets up components and state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [pokemon, setPokemon] useState(["bulbasaur", "charmander"])
+
   return (
     null
   );
---
+++ src/components/PokemonList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PokemonList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PokemonList
