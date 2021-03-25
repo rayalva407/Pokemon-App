3e1088656bf17e27b75722ad8415b20745176e78
rayalva407 <rayalva407@gmail.com>
2021-03-25 15:07:30 -0600

    calls the api to get pokemon names

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonList from './components/PokemonList'
+import axios from 'axios'
 
 function App() {
 
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"])
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+      setPokemon(res.data.results.map(p => p.name))
+    }) 
+  }, [])
 
   return (
     <PokemonList pokemon={pokemon} />
